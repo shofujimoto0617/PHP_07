cd445e3f2398b9712bb55fe94c463ef0aefcbe1a
shofujimoto0617 <shofujimoto017@gmail.com>
2021-01-09 17:35:02 +0900

    add SoftDelete to Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,33 +6,36 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use Auth;
 use Illuminate\Support\Carbon;
-//* save DBにデーターを登録③の場合必要 ↓↓
+/* save DBにデーターを登録③の場合必要 ↓↓ */
 use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
-    //* Category AllCat(index)
+    /* Category AllCat(index) */
     public function AllCat(){
 
-        //* DBからuserの値を直接取得(投稿順) ↓↓
+        /* DBからuserの値を直接取得(投稿順) ↓↓ */
         // $categories = DB::table('categories')
         //         ->join('users','categories.user_id','users.id')
         //         ->select('categories.*','users.name')
         //         ->latest()->paginate(5);
 
-        //* Modelから値取得(投稿順) ↓↓
+        /* Modelから値取得(投稿順) ↓↓ */
         $categories = Category::latest()->paginate(5);
 
-        //* DBから値取得(投稿順) ↓↓
+        $trachCat = Category::onlyTrashed()->latest()->paginate(3);
+
+
+        /* DBから値取得(投稿順) ↓↓ */
         // $categories = DB::table('categories')->latest->paginate(5);
 
-        return view('admin.category.index', compact('categories'));
+        return view('admin.category.index', compact('categories','trachCat'));
     }
 
-    //* Category AddCat(save)
+    /* Category AddCat(save) */
     public function AddCat(Request $request){
 
-        //* validate
+        /* validate */
         $validated = $request->validate([
             'category_name' => 'required|unique:categories|max:255',
             
@@ -43,22 +46,22 @@ class CategoryController extends Controller
             
         ]);
 
-        //* Category Save ↓↓↓ (3 types)
+        /* Category Save ↓↓↓ (3 types) */
 
-        //* DBにデーターを登録（方法①）
+        /* DBにデーターを登録（方法①）*/
         Category::insert([
             'category_name' => $request->category_name,
             'user_id' => Auth::user()->id,
             'created_at' => Carbon::now()
         ]);
 
-        //* DBにデーターを登録（方法②）
+        /* DBにデーターを登録（方法②） */
         // $category = new Category;
         // $category->category_name = $request->category_name;
         // $category->user_id = Auth::user()->id;
         // $category->save();
 
-        //* DBにデーターを登録（方法③）
+        /* DBにデーターを登録（方法③）*/
         // $data = array();
         // $data['category_name'] = $request->category_name;
         // $data['user_id'] = Auth::user()->id;
@@ -67,32 +70,38 @@ class CategoryController extends Controller
         return Redirect()->back()->with('success','Category Inserted Successfull');
     }
 
-
+    /* Category AddCat(edit) */
     public function Edit($id){
-        //* Modelから値取得 ↓↓
+        /* Modelから値取得（方法①） ↓↓ */
         // $categories = Category::find($id);
 
-        //* DBから値取得 ↓↓
+        /* DBから値取得（方法②） ↓↓ */
         $categories = DB::table('categories')->where('id',$id)->first();
 
         return view('admin.category.edit',compact('categories'));
 
     }
 
+    /* Category AddCat(update) */
     public function Update(Request $request ,$id){
 
-        //* ModelからUpdate ↓↓
+        /* ModelからUpdate（方法①) */
         // $update = Category::find($id)->update([
         //     'category_name' => $request->category_name,
         //     'user_id' => Auth::user()->id
         // ]);
 
-        //* DBへ直接Update ↓↓
+        /* DBへ直接Update（方法②) */
         $data = array();
         $data['category_name'] = $request->category_name;
         $data['user_id'] = Auth::user()->id;
         DB::table('categories')->where('id',$id)->update($data);
 
         return Redirect()->route('all.category')->with('success','Category Updated Successfull');
+    }
+
+    public function SoftDelete($id){
+        $delete = Category::find($id)->delete();
+        return Redirect()->back()->with('success','Category Soft Delete Successfully');
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $fillable = [
         'user_id',
         'category_name',
